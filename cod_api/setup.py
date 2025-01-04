20260d5e75a02751f87208b3b551f75e111a7096
Rim <mrcodblackops10>
2025-01-04 06:41:51 -0500

    maint(): slim directory

---
+++ cod_api/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages 
+setup(
+    name='cod_api', version='2.0.1', 
+    packages=find_packages(), 
+    install_requires=[],
+)
--- src/cod-python-api/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-requirements = ["asyncio", "aiohttp", "datetime", "requests", "uuid", "urllib3", "enum34"]
-
-setup(
-    name="cod_api",
-    packages=['cod_api'],
-    install_requires=requirements
-)
